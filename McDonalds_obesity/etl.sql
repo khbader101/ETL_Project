a643445d65cc57afca250839c1cc92652380d6ab
Kendall Sisk <69765842+Klsisk@users.noreply.github.com>
2020-12-14 18:56:45 -0500

    final commit

--- McDonalds_obesity/etl.sql
+++ McDonalds_obesity/etl.sql
@@ -22,4 +22,19 @@ from mcdonalds m
 inner join cities c on m.city = c.city
 group by m.city
 
+-- Query for cities with most McDonalds' locations
+select count(*) as McDonalds_count, m.city, round(avg(c.bmi)) as BMI, round(avg(c.population)) as Population
+from mcdonalds m
+inner join cities c on m.city = c.city
+group by m.city
+order by McDonalds_count desc
+
+-- Query for cities with highest BMI
+select count(*) as McDonalds_count, m.city, round(avg(c.bmi)) as BMI, round(avg(c.population)) as Population
+from mcdonalds m
+inner join cities c on m.city = c.city
+group by m.city
+order by BMI desc
+
+
 
